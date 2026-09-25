4f638165e21a94da9d0979dc66167fb512a58da9
Gaurav kumar <gaurav@Gauravs-MacBook-Air.local>
2026-09-25 11:34:08 +0530

    first chages

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import './Hero.css'
+import React from 'react'
+import Navbar from './Navbar.jsx'
+import Hero from './Herio.jsx'
+import Depoly from './comonents/Depoly.jsx'
+import Training from './comonents/Training.jsx'
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Depoly />
+      <Training/>
+      </>
+  )
+}
+
+export default App
